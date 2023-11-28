cb8e37bc184ba65b984bea30b05f693d01f0c406
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-11-28 14:55:21 -0600

    Set up form and a function called addAirline

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,32 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+
+  const [airlineInput, setAirlineInput] = useState('');
+  // Selector
+  const airlineList = useSelector((store) => store.airlineList);
+  // Create a way to communicate with the store
+  const dispatch = useDispatch();
+
+  const addAirline = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: 'ADD_NEW_AIRLINE',
+      payload: airlineInput
+    })
+  }
 
   return (
     <div>
       <h1>Redux Airport</h1>
-      <form>
+      <form onSubmit={addAirline}>
         <input 
-        placeholder='Airline Name' />
+          placeholder='Airline Name'
+          value={airlineInput}
+          onChange={(e) => setAirlineInput(e.target.value)}
+          type="text"
+        />
         <button>Add Airline</button>
       </form>
       <table>{/* Airlines should be listed here */}</table>
